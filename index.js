5f3d952e7c4d45edb65895e4460a68a7a026bf73
piotrmigas <piotr.migas@icloud.com>
2023-08-16 11:30:39 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import expressPlayground from 'graphql-playground-middleware-express';
@@ -11,7 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
-app.use(cors());
+
 app.use(
   '/graphql',
   createHandler({
